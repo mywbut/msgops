f8d96707a047ea19cb9195fe13774ba1a1ddb971
Chandrakant Prasad <chandrakantprasad@MacBook-Air.local>
2026-03-23 13:22:14 +0530

    Fix missing Log import in WebhookController

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Jobs\ProcessWhatsAppWebhook;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class WebhookController extends Controller
 {
