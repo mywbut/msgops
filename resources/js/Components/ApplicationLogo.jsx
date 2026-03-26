9699d6fdd57113647f76e8a9eb522143168b9d12
Chandrakant Prasad <chandrakantprasad@MacBook-Air.local>
2026-03-26 16:02:11 +0530

    Update application logo to logo.png

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -2,7 +2,7 @@ export default function ApplicationLogo(props) {
     return (
         <img 
             {...props} 
-            src="/images/logo-concept.png" 
+            src="/images/logo.png" 
             alt="MsgOps Logo" 
             className={`${props.className} object-contain`}
         />
